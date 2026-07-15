abc8641353c83036758674154c872e8b7619a9f5
atisss <aatishkumarsahu05012006@gmail.com>
2026-07-15 16:20:41 +0530

    Changed training command.

--- Vision/train.py
+++ Vision/train.py
@@ -7,18 +7,18 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         print(f"Training on GPU: {torch.cuda.get_device_name(0)}")
 
-    # model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8n.pt")
 
-    # model.train(
-    #     data="RoadVehiclesYOLODatasetPro/data.yaml", 
-    #     epochs=50, 
-    #     save=True,
-    #     device=0,
-    #     batch=16,
-    #     imgsz=640,
-    #     workers=2,
-    #     patience=10
-    # )
+    model.train(
+        data="RoadVehiclesYOLODatasetPro/data.yaml", 
+        epochs=50, 
+        save=True,
+        device=0,
+        batch=16,
+        imgsz=640,
+        workers=2,
+        patience=10
+    )
     
-    model = YOLO("runs/detect/train-6/weights/last.pt")
-    model.train(resume = True)
+    # model = YOLO("Enter last.pt")
+    # model.train(resume = True)
